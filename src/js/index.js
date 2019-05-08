94b736ea3af92e66a76651652cc9fc3bf621ff5a
Tiago Pereira <correio.tiago@gmail.com>
2019-05-08 23:29:43 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+// Global app controller
+// import Search from './models/Search';
+// import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+/**
+ * ### CONTROLLER
+ **/
---
+++ src/js/views/base.js
@@ -0,0 +1,25 @@
+export const elements = {
+    // searchForm: document.querySelector('.search'),
+    // searchInput: document.querySelector('.search__field'),
+};
+
+export const elementStrings = {
+    loader: 'loader'
+};
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div
+    `;
+
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) loader.parentElement.removeChild(loader);
+};
