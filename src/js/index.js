b877b790f3616a004038826a09c79d50c5ecd6c4
Tiago Pereira <correio.tiago@gmail.com>
2019-05-11 16:50:45 +0100

    update styles

--- src/js/index.js
+++ src/js/index.js
@@ -17,4 +17,52 @@ const state = {};
  * ### CONTROLLER
  **/
 
-console.log('asdfsdasdf');
+console.log(elements.sections);
+
+// const menu = elements.menu;
+// menu.forEach(option =>
+//     option.addEventListener('click', () => {
+//         console.log(option.childNodes[1].href);
+//     })
+// );
+
+const toggleButton = elements.toggleButton;
+const table = elements.table;
+const chart = elements.chart;
+
+const controlMenu = () => {
+    const id = window.location.hash.replace('#', '');
+    const sections = [...elements.sections];
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+    let section;
+    if (id) {
+        console.log(id);
+        console.log(sections[0].className);
+        section = sections.find(section => section.className == `section-${id}`);
+        console.log(section);
+    } else {
+        section = sections.find(section => section.className == `section-about`);
+    }
+    section.style.display = 'flex';
+
+    checkCheckBoxValue();
+};
+
+const checkCheckBoxValue = () => {
+    console.log(toggleButton.checked);
+    if (toggleButton.checked) {
+        table.style.display = 'none';
+        chart.style.display = 'block';
+    } else {
+        table.style.display = 'block';
+        chart.style.display = 'none';
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlMenu));
+
+toggleButton.addEventListener('change', () => {
+    checkCheckBoxValue();
+});
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,6 +1,11 @@
 export const elements = {
     // searchForm: document.querySelector('.search'),
     // searchInput: document.querySelector('.search__field'),
+    menu: document.querySelectorAll('.topbar__item'),
+    sections: document.querySelectorAll('section'),
+    toggleButton: document.querySelector('#checkbox-toggle'),
+    table: document.querySelector('.data-box__table'),
+    chart: document.querySelector('.data-box__chart')
 };
 
 export const elementStrings = {
